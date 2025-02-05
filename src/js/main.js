d48d655965f55afa8781537463a34c6f60e6bcce
Martin Ferrero-Thompson <153296438+Martin-Ferrero-Thompson@users.noreply.github.com>
2025-02-05 08:17:53 +0100

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import my DailyUI custom CSS
+import '../scss/style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
