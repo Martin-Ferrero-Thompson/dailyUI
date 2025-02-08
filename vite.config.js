becadc7033b547c733d2919ed6257a78290e8e77
Martin Ferrero-Thompson <153296438+Martin-Ferrero-Thompson@users.noreply.github.com>
2025-02-08 17:32:50 +0100

    Updated Daily challenge structures

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,51 @@
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import path from 'path';
 
-export default {
-  root: resolve(__dirname, 'src'),
+export default defineConfig({
+  root: 'src',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@images': path.resolve(__dirname, './src/images'), // Alias for images folder
+    },
   },
   server: {
-    port: 8080
+    port: 8081,
   },
-  publicDir: 'assets'
+  rollupOptions: {
+    input: {
+      main: path.resolve(__dirname, 'src/index.html'),
+      ...getHtmlFiles('./src'),
+    },
+  },
+});
+
+function getHtmlFiles(dir) {
+  const fs = require('fs');
+  const path = require('path');
+  const entries = {};
+
+  function traverse(currentDir) {
+    const files = fs.readdirSync(currentDir);
+
+    files.forEach(file => {
+      const fullPath = path.join(currentDir, file);
+      const stats = fs.statSync(fullPath);
+
+      if (stats.isDirectory()) {
+        traverse(fullPath);
+      } else if (file.endsWith('.html')) {
+        const relativePath = path.relative('./src', fullPath);
+        const name = relativePath.replace(/\.html$/, '').replace(/\\/g, '/');
+        entries[name] = fullPath;
+      }
+    });
+  }
+
+  traverse(dir);
+  return entries;
 }
