0b7d03ae660b05437356b65bca579e7f4c7f47fa
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-05-19 14:02:42 +0200

    Update wiith new styling

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-// Import our custom CSS
-import '../scss/styles.scss'
-
-// Import my DailyUI custom CSS
-import '../scss/style.scss'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
